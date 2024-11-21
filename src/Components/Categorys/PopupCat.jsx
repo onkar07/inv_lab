5965ad41b99129712a95bd1e80ef0347a1714d99
Dhirajpatil7721 <dhirajpatil7721@gmail.com>
2024-11-21 17:20:23 +0530

    ok

--- src/Components/Categorys/PopupCat.jsx
+++ src/Components/Categorys/PopupCat.jsx
@@ -1,12 +1,12 @@
 import React, { useRef } from 'react'
 import './PopupCat.css'
 
-function PopupCat({onClose}) {
+function PopupCat({ onClose }) {
 
-    const popRef=useRef();
+    const popRef = useRef();
 
-    const closePop=(e)=>{
-        if(popRef.current === e.target){
+    const closePop = (e) => {
+        if (popRef.current === e.target) {
             onClose();
         }
     }
@@ -32,8 +32,8 @@ function PopupCat({onClose}) {
                 <br />
                 <div className="working">
 
-                <label className='lable3'>Working </label>
-                <input className='in3' type="checkbox" />
+                    <label className='lable3'>Working </label>
+                    <input className='in3' type="checkbox" />
                 </div>
 
             </div>
@@ -41,18 +41,10 @@ function PopupCat({onClose}) {
 
                 {/* <button>Submit</button> */}
                 <button id="btn1" className="noselect"><span class="text">Submit</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg></span></button>
-                               
+
                 {/* <button>Delete</button> */}
                 <button onClick={onClose} id="btn2" className="noselect"><span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
             </div>
-
-            {/* <div class="toggle-button-cover">
-        <div id="button-3" class="button r">
-          <input class="checkbox" type="checkbox"/>
-          <div class="knobs"></div>
-          <div class="layer"></div>
-        </div>
-      </div> */}
 
         </div>
     )
--- src/Components/Equipment/ViewAll.jsx
+++ src/Components/Equipment/ViewAll.jsx
@@ -4,15 +4,32 @@ import PopDelete from './PopDelete'
 import Addequipment from './Addequipment'
 
 function ViewAll() {
-    const [pop,setPop]=useState(false)
-    const [pope,setPope]=useState(false)
+    const [pop, setPop] = useState(false)
+    const [pope, setPope] = useState(false)
+
+    const items = [
+        { id: 1, name: "Multimeter", qty: "5", status: "ok", date: "19/02/2001" },
+        { id: 2, name: "Oscilloscope", qty: "3", status: "ok", date: "15/08/2020" },
+        { id: 3, name: "Power Supply", qty: "8", status: "maintenance", date: "01/12/2022" },
+        { id: 4, name: "Soldering Iron", qty: "10", status: "ok", date: "25/05/2023" },
+        { id: 5, name: "Thermal Camera", qty: "2", status: "ok", date: "12/10/2021" },
+        { id: 6, name: "Clamp Meter", qty: "7", status: "ok", date: "03/03/2023" },
+        { id: 7, name: "Signal Generator", qty: "4", status: "calibration", date: "20/06/2020" },
+        { id: 8, name: "Frequency Counter", qty: "6", status: "ok", date: "11/11/2022" }
+    ];
+
+
     return (
         <div id='alltable' >
-            <h2 style={{marginBottom:'30px', marginTop:'30px'}}>Equipment Equipments List </h2>
+            <h2 style={{ marginBottom: '30px', marginTop: '30px' }}>Equipment Equipments List </h2>
+
+            <h2 className='h2add' onClick={() => setPope(true)}><span className='span'> +  </span>  Add New </h2>
+
 
             <div className="table">
 
                 <table>
+
                     <tr>
                         <th>ID</th>
                         <th>NAME</th>
@@ -21,114 +38,34 @@ function ViewAll() {
                         <th>DATE</th>
                         <th>ACTION</th>
                     </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  |
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  |
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  |
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  |
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  |
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  |
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  | 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  | 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  |
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
-                    <tr>
-                        <th>1</th>
-                        <th>Multimiter</th>
-                        <th>5</th>
-                        <th>yes</th>
-                        <th>19/02/2001</th>
-                        <th style={{color:'blue'}}> 
-                        <span style={{cursor:"pointer"}} onClick={()=>setPope(true)}  >Edit</span>  |
-                        <span style={{cursor:"pointer"}} onClick={()=>setPop(true)}>Delete</span></th>
-                    </tr>
 
-
-                    
+                    {/* <tr>
+                        <td>1</td>
+                        <td>Multimiter</td>
+                        <td>5</td>
+                        <td>yes</td>
+                        <td>19/02/2001</td>
+                        <td style={{ color: 'blue' }}>
+                            <span style={{ cursor: "pointer" }} onClick={() => setPope(true)}  >Edit</span>  |
+                            <span style={{ cursor: "pointer" }} onClick={() => setPop(true)}>Delete</span></td>
+                    </tr> */}
+                    {items.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.qty}</td>
+                            <td>{item.status}</td>
+                            <td>{item.date}</td>
+                            <td style={{ color: 'blue' }}>
+                                <span style={{ cursor: "pointer" }} onClick={() => setPope(true)}  >Edit</span>  |
+                                <span style={{ cursor: "pointer" }} onClick={() => setPop(true)}>Delete</span></td>
+                        </tr>
+                    ))}
                 </table>
 
             </div>
-{pop &&  <PopDelete onhide={() => setPop(false)} />}
-{pope && <Addequipment ongo={()=>setPope(false)} /> }
+            {pop && <PopDelete onhide={() => setPop(false)} />}
+            {pope && <Addequipment ongo={() => setPope(false)} />}
 
         </div>
     )
