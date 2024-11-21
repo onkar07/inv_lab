7b128c122cae6e35d7c6ee98d163caff96c06838
Dhirajpatil7721 <dhirajpatil7721@gmail.com>
2024-11-21 13:43:04 +0530

    responsive

--- src/Components/Categorys/Category.jsx
+++ src/Components/Categorys/Category.jsx
@@ -7,24 +7,24 @@ import PopupCat from './PopupCat'
 
 function Category() {
     const [show, setShow] = useState(false)
-   
+
     return (
         <div className='cat' style={{ width: '100%' }}>
 
-            
+
             <div className="name">
                 <p>Physics Laboratory</p>
             </div>
 
             <div className="add " onClick={() => { setShow(true) }}>
                 <div className="addtxt">
-                    <i className="addico fa-solid fa-circle-plus"><p style={{ fontFamily: 'Arial' }}> New Category</p>   </i>
+                    <i className="addico fa-solid fa-circle-plus"><p style={{ fontFamily: 'Arial',    paddingtop: "4px" }}> New Category</p>   </i>
                 </div>
 
                 <p className='p'  >Physics Lab Equipment Category</p>
             </div>
 
-            <div className=" cardsec col-12 col-sm-6 col-md-4 col-lg-4 ">
+            <div  className=" cardsec col-12 col-sm-6 col-md-4 col-lg-4 ">
                 <div className="row">
                     <Card />
                     <Card />
